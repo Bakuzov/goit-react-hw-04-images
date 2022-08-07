ed0076f5bc04d151a2ad3056658e9228ca9342b2
Mike Bakuzov <bakuzov@gmail.com>
2022-08-07 23:17:06 +0300

    new key

--- src/fetchApi/fetchApi.js
+++ src/fetchApi/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/';
-const API_KEY = '28362853-f4179f2b0382cbb2305a6e9dc';
+const API_KEY = '6205723-a701a2b99407455c76c95e325';
 export const fetchApi = (search, page) => {
   return axios.get(
     `api/?key=${API_KEY}&q=${search}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
